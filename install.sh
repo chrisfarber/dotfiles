d80def348ebf3a6a17007b6545c6d6538e22935c
Chris Farber <chris@chrisfarber.net>
2021-06-24 20:00:02 -0400

    sketch out substeps

--- install.sh
+++ install.sh
@@ -1,3 +1,12 @@
 #!/usr/bin/env sh
 
-echo hello world
+SOURCE="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+DEST=~
+
+function step() {
+  cd "$SOURCE/$1"
+  ./install.sh
+  cd "$SOURCE"
+}
+
+step "zsh"
