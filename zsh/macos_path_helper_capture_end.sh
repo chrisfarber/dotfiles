ffa2a3d543b4a3272c5e3e6e8a06fa766228238d
Chris Farber <chris@chrisfarber.net>
2021-06-26 23:08:32 -0400

    macos path hack works with nested shells

--- zsh/macos_path_helper_capture_end.sh
+++ zsh/macos_path_helper_capture_end.sh
@@ -1,2 +1,13 @@
 # capture the paths after all our init
-path_before_path_helper=($path[@])
+
+if [[ -z $KNOWN_USER_ENV_PATH ]]; then
+  local path_after_user_env=($path[@])
+  local user_env_paths=()
+  for dir in $path_after_user_env; do
+    if [[ $path_before_user_env[(Ie)$dir] -gt 0 ]] continue
+    user_env_paths+=($dir)
+  done
+  unset path_after_user_env
+  unset user_env_paths
+  export KNOWN_USER_ENV_PATH=${(j.:.)user_env_paths}
+fi
--- zsh/macos_path_helper_fix.sh
+++ zsh/macos_path_helper_fix.sh
@@ -1,25 +1,15 @@
 #path_before_user_env
 #path_before_path_helper
-path_after_path_helper=($path[@])
 
-
-path=()
-
-# get the user defined paths
-for dir in $path_before_path_helper; do
-  # skip the path if it exists in path_before_user_env
-  if [[ $path_before_user_env[(Ie)$dir] -gt 0 ]] continue
-  path+=($dir)
+disturbed_path=($path[@])
+PATH=$KNOWN_USER_ENV_PATH
+for dir in $disturbed_path; do
+  if [[ $path[(Ie)$dir] -eq 0 ]]; then
+    path+=($dir)
+  fi
 done
 
-for dir in $path_after_path_helper; do
-  if [[ $path[(Ie)$dir] -gt 0 ]] continue
-  path+=($dir)
-done
-
-# path=($new_paths[@])
-
-unset dir
+export path
 unset path_before_user_env
-unset path_before_path_helper
-unset path_after_path_helper
+unset disturbed_path
+unset dir
