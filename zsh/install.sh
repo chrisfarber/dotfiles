8892c29fe1659467bbc0b5f26bd060a8dba86e57
Chris Farber <chris@chrisfarber.net>
2021-06-26 21:01:04 -0400

    no need for the workaround if not macos

--- zsh/install.sh
+++ zsh/install.sh
@@ -10,7 +10,9 @@ zsh_env 01 "local-bin.sh"
 
 zsh_env 05 "version_managers.sh"
 
-# here is the magic that works around /usr/libexec/path_helper:
-zsh_env 00 "macos_path_helper_capture_start.sh"
-zsh_env 99 "macos_path_helper_capture_end.sh"
-zsh_rc 99 "macos_path_helper_fix.sh"
+if macos; then
+  # here is the magic that works around /usr/libexec/path_helper:
+  zsh_env 00 "macos_path_helper_capture_start.sh"
+  zsh_env 99 "macos_path_helper_capture_end.sh"
+  zsh_rc 99 "macos_path_helper_fix.sh"
+fi
