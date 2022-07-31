fdebefc08aee1eb60093b62d49c6ad3ead0b5c21
Chris Farber <chris@chrisfarber.net>
2022-07-31 16:03:55 -0400

    remove macos keychain stuff

--- ssh/install.sh
+++ ssh/install.sh
@@ -6,7 +6,3 @@ symlink "config" ".ssh/config"
 
 # TODO: make a patch function to ensure it exists?
 copy_once "id_ed25519.pub" ".ssh/authorized_keys"
-
-if macos; then
-  symlink "keychain" ".ssh/config.d/keychain"
-fi
