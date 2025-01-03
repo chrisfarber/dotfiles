67c74ab351986959e64d73755c2df8050c688765
Chris Farber <cfarber@fastly.com>
2025-01-03 09:18:35 -0500

    remove macos /usr/libexec/path_helper workaround
    Unfortunately, it's always been a bit finnicky. Lately
    it's been causing problems, and, i'm not sure the
    problem that it is trying to solve is still relevant to
    my setup.

--- zsh/install.sh
+++ zsh/install.sh
@@ -15,7 +15,11 @@ zsh_env 01 "local-bin.sh"
 
 if macos; then
   # here is the magic that works around /usr/libexec/path_helper:
-  zsh_env 00 "macos_path_helper_capture_start.sh"
-  zsh_env 99 "macos_path_helper_capture_end.sh"
-  zsh_rc 99 "macos_path_helper_fix.sh"
+  # zsh_env 00 "macos_path_helper_capture_start.sh"
+  # zsh_env 99 "macos_path_helper_capture_end.sh"
+  # zsh_rc 99 "macos_path_helper_fix.sh"
+
+  unsymlink ".zshenv.d/00-macos_path_helper_capture_start.sh"
+  unsymlink ".zshenv.d/99-macos_path_helper_capture_end.sh"
+  unsymlink ".zshrc.d/99-macos_path_helper_fix.sh"
 fi
--- zsh/macos_path_helper_capture_start.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env zsh
-
-# capture the paths before our user init
-path_before_user_env=($path[@])
