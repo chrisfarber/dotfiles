3e7ec9b0f050e881cc479ba81e6cfc116b448015
Chris Farber <cfarber@fastly.com>
2025-01-03 09:32:11 -0500

    renounce ownership of user ssh config

--- ssh/install.sh
+++ ssh/install.sh
@@ -1,8 +1,9 @@
 directory ".ssh"
-directory ".ssh/config.d"
-directory ".ssh/keys"
 
-symlink "config" ".ssh/config"
+# 2025-01-03 - disabling this, because various tools keep polluting
+# the .ssh/config file and I don't have much customization in it anyway.
+
+# symlink "config" ".ssh/config"
 
 # TODO: make a patch function to ensure it exists?
 copy_once "id_ed25519.pub" ".ssh/authorized_keys"
