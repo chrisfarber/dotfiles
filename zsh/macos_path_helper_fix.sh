e42d3f1501f8218a8247c33597f21f639b1ca6d4
Chris Farber <cfarber@fastly.com>
2025-09-17 17:55:26 -0400

    macos path_helper fix only applies once

--- zsh/macos_path_helper_fix.sh
+++ zsh/macos_path_helper_fix.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env zsh
 
+# Skip if this fix has already been applied
+if [[ -n "$CFARBER_MACOS_PATH_HELPER_WORKAROUND_COMPLETE" ]]; then
+  return 0
+fi
+
 disturbed_path=($path[@])
 PATH=$KNOWN_USER_ENV_PATH
 for dir in $disturbed_path; do
@@ -11,3 +16,6 @@ done
 export path
 unset disturbed_path
 unset dir
+
+# Mark that this fix has been applied
+export CFARBER_MACOS_PATH_HELPER_WORKAROUND_COMPLETE=1
