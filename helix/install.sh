43e7c5736ec7134b46e38379156304e5e80f72b3
Chris Farber <cfarber@fastly.com>
2026-06-16 08:57:28 -0400

    helix: monokai pro light, vibe coded by claude

--- helix/install.sh
+++ helix/install.sh
@@ -1,6 +1,8 @@
 directory_with_mode ".config" 700
 directory_with_mode ".config/helix" 700
+directory_with_mode ".config/helix/themes" 700
 
 symlink "config.toml" ".config/helix/config.toml"
 symlink "languages.toml" ".config/helix/languages.toml"
 symlink "snippets" ".config/helix/snippets"
+symlink "monokai_pro_light.toml" ".config/helix/themes/monokai_pro_light.toml"
